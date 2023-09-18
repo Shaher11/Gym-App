c4caf721f9ceec4ff88e7d93e213241e7de97515
Mohamed Ramadan <m7mdrama1245@gmail.com>
2023-09-18 16:13:09 +0300

    add custom middleware to handle routes access

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,15 @@ Route::get('/dashboard', DashboardController::class)->middleware(['auth'])->name
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
-})->middleware(['auth'])->name('admin.dashboard');
+})->middleware(['auth', 'role:Admin'])->name('admin.dashboard');
 
 Route::get('/instructor/dashboard', function () {
     return view('instructor.dashboard');
-})->middleware(['auth'])->name('instructor.dashboard');
+})->middleware(['auth', 'role:Instructor'])->name('instructor.dashboard');
 
 Route::get('/member/dashboard', function () {
     return view('member.dashboard');
-})->middleware(['auth'])->name('member.dashboard');
+})->middleware(['auth', 'role:Member'])->name('member.dashboard');
 
 
 Route::middleware('auth')->group(function () {
