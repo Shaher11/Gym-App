9aca6d56f870f7e6915a1c2a45858e24e7ab0010
Mohamed Ramadan <m7mdrama1245@gmail.com>
2023-09-24 19:28:27 +0300

    sending emails to members when canceling class

--- app/Listeners/NotifyClassCanceled.php
+++ app/Listeners/NotifyClassCanceled.php
@@ -3,9 +3,11 @@
 namespace App\Listeners;
 
 use App\Events\ClassCanceled;
+use App\Mail\ClassCancelMail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class NotifyClassCanceled
 {
@@ -22,7 +24,17 @@ class NotifyClassCanceled
      */
     public function handle(ClassCanceled $event): void
     {
-        $scheduledClass = $event->scheduledClass;
-        Log::info($scheduledClass);
+
+        // Log::info($scheduledClass);
+        $members = $event->scheduledClass->members();
+
+        $className = $event->scheduledClass->classType->name;
+        $classDateTime = $event->scheduledClass->date_time;
+
+        $details = compact('className','classDateTime');
+
+        $members->each(function($user) use ($details){
+            Mail::to($user)->send(new ClassCancelMail($details));
+        });
     }
 }
