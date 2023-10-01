1a556e04ea4e183eb9040afe6f3d82527a117099
Mohamed Ramadan <m7mdrama1245@gmail.com>
2023-10-01 11:36:57 +0300

    send notification when the class canceled

--- app/Listeners/NotifyClassCanceled.php
+++ app/Listeners/NotifyClassCanceled.php
@@ -4,7 +4,9 @@ namespace App\Listeners;
 
 use App\Events\ClassCanceled;
 use App\Mail\ClassCancelMail;
+use App\Notifications\ClassCanceledNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
@@ -26,15 +28,19 @@ class NotifyClassCanceled
     {
 
         // Log::info($scheduledClass);
-        $members = $event->scheduledClass->members();
+
+        // $members = $event->scheduledClass->members();
+        $members = $event->scheduledClass->members()->get();
 
         $className = $event->scheduledClass->classType->name;
         $classDateTime = $event->scheduledClass->date_time;
 
         $details = compact('className','classDateTime');
 
-        $members->each(function($user) use ($details){
-            Mail::to($user)->send(new ClassCancelMail($details));
-        });
+        // $members->each(function($user) use ($details){
+        //     Mail::to($user)->send(new ClassCancelMail($details));
+        // });
+
+        Notification::send($members, new ClassCanceledNotification($details));
     }
 }
